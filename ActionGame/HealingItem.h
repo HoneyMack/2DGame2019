eb113a092482eddf882cfd0fdbf978fcb097d740
Syuuki <syuuki0927@gmail.com>
2019-10-04 17:35:55 +0900

    change branch

---
+++ ActionGame/HealingItem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Mob.h"
+#include "Object.h"
+#include "Rect.h"
+
+class HealingItem :public Mob
+{
+public:
+	static int PicHandle;
+	Rect rect = Rect(HISIZE_X,HISIZEY,&this->)
+};
+
